1b0ec1b99a98e43f59a47a4055e78bdba7efed26
Đỗ Đạt <ddatdt12@gmail.com>
2022-05-31 17:07:25 +0700

    skill tag crud api

--- src/routes/admin/skill.js
+++ src/routes/admin/skill.js
@@ -5,7 +5,12 @@ const { skillController } = require('../../controllers/admin');
 
 router
 	.route('/')
-	.get(skillController.getAllSkills)
-	.post(skillController.createSkills);
+	.get(skillController.getSkills)
+	.post(skillController.createSkill);
+router.post('/batch', skillController.createBatchSkills);
+router
+	.route('/:id')
+	.put(skillController.updateSkill)
+	.delete(skillController.deleteSkill);
 
 module.exports = router;
